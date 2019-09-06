24a74f9f2652bc44bf919a95700b97eee65d2d21
frinhani <frinhani.frinhani@gmail.com>
2019-09-06 00:30:14 -0300

    [vda-02] - MVC, form cadastro evento

--- src/main/java/br/com/eventoapp/EventoappApplication.java
+++ src/main/java/br/com/eventoapp/EventoappApplication.java
@@ -1,8 +1,10 @@
-package br.com.eventoapp.eventoapp;
+package br.com.eventoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"br.com.eventoapp.controllers"})
 @SpringBootApplication
 public class EventoappApplication {
 
