dca9c4fdbebfb3cb89ba12ddf9b80e0b8935e449
frinhani <frinhani.frinhani@gmail.com>
2019-09-09 21:27:05 -0300

    [vda-04] - Save Evento

--- src/main/java/br/com/eventoapp/controllers/EventoController.java
+++ src/main/java/br/com/eventoapp/controllers/EventoController.java
@@ -1,15 +1,29 @@
 package br.com.eventoapp.controllers;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.eventoapp.models.Evento;
+import br.com.eventoapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
 	
-	@RequestMapping("/cadastrarEvento")
-	public String form() {
+	@Autowired
+	EventoRepository eventoRepository;
+	
+	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.GET)
+	public String getForm() {
+		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.POST)
+	public String saveForm(Evento evento) {
+		
+		eventoRepository.save(evento);
+		
 		return "evento/formEvento";
 	}
 
