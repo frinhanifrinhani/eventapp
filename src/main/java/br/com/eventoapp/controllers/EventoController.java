91db9d1cc12d02e826920f76c4800a80592c2860
frinhani <frinhani.frinhani@gmail.com>
2019-09-09 21:27:05 -0300

    [vda-05] - List Eventos

--- src/main/java/br/com/eventoapp/controllers/EventoController.java
+++ src/main/java/br/com/eventoapp/controllers/EventoController.java
@@ -1,16 +1,43 @@
 package br.com.eventoapp.controllers;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.eventoapp.models.Evento;
+import br.com.eventoapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
 	
-	@RequestMapping("/cadastrarEvento")
-	public String form() {
+	@Autowired
+	EventoRepository eventoRepository;
+	
+	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.GET)
+	public String getForm() {
 		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.POST)
+	public String saveForm(Evento evento) {
+		
+		eventoRepository.save(evento);
+		
+		return "evento/formEvento";
+	}
+	
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos(){
+		ModelAndView modelAndView = new ModelAndView("index");
+		Iterable<Evento> eventos = eventoRepository.findAll();
+		modelAndView.addObject("eventos",eventos);
+		
+		return modelAndView;
+		
+		
 	}
 
 }
--- src/main/java/br/com/eventoapp/models/Evento.java
+++ src/main/java/br/com/eventoapp/models/Evento.java
@@ -1,11 +1,33 @@
 package br.com.eventoapp.models;
 
-public class Evento {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable {
+
+	private static final long serialVersionUID = 1l;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long codigo;
+	
 	private String nome;
 	private String local;
 	private String data;
 	private String horario;
+	
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
 	public String getNome() {
 		return nome;
 	}
