7d0519702d1a917edf5ab9a5ddc808f107b9242d
frinhani <frinhani.frinhani@gmail.com>
2019-09-12 21:37:35 -0300

    [vda-08] - Detalhe evento

--- src/main/java/br/com/eventoapp/controllers/EventoController.java
+++ src/main/java/br/com/eventoapp/controllers/EventoController.java
@@ -2,6 +2,7 @@ package br.com.eventoapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -37,6 +38,15 @@ public class EventoController {
 		
 		return modelAndView;
 		
+	}
+	
+	@RequestMapping("detalheEvento/{codigo}")
+	public ModelAndView detalheEvento(@PathVariable("codigo") long 	codigo ) {
+		Evento evento = eventoRepository.findByCodigo(codigo);
+		ModelAndView modelAndView = new ModelAndView("evento/detalheEvento");
+		modelAndView.addObject("evento", evento);
+		
+		return modelAndView; 
 		
 	}
 
--- src/main/java/br/com/eventoapp/repository/EventoRepository.java
+++ src/main/java/br/com/eventoapp/repository/EventoRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import br.com.eventoapp.models.Evento;
 
 public interface EventoRepository extends CrudRepository<Evento, String> {
+	
+	Evento findByCodigo(Long codigo);
 
 }
